462ab0db880558c53a3f469fb7ed5008664f4000
vadimborovik <v.borovuk@softum.io>
2026-09-05 14:15:42 +0300

    update

--- server/src/services/tokenService.js
+++ server/src/services/tokenService.js
@@ -3,8 +3,8 @@ const db = require("../config/db");
 
 class TokenService {
   generateTokens(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '10s'})
-    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '20s'})
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '1d'})
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '10d'})
 
     return {accessToken, refreshToken}
   }
